ddad0f2f49fbae21383dc20ad9825efe070b4929
Ilir-Prog <iliri.isufi@gmail.com>
2025-08-20 01:00:56 +0200

    Updated testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -9,23 +9,16 @@ export interface Testimonial {
 
 export const testimonials: Testimonial[] = [
   {
-    id: 2,
-    name: "Walter Ferrari",
-    role: "Client régulier",
-    content: "Very efficient barber with a warm welcome, and a good price for a cut of CHF 25.- Ideal if you're looking for good value for money     in the center of Lausanne.",
-    rating: 5,
-    image: "https://lh3.googleusercontent.com/a-/ALV-UjUuwHljpOP57ZtJSpQlFyMdDBWC6ymoUgGLOKXVmrGuuIa1FzGA=w90-h90-p-rp-mo-ba4-br100"
-  },
-  {
     id: 1,
-    name: "Trey Davis",
+    name: "Gauthier R",
     role: "Client régulier",
-    content: "The gentleman is very friendly and the atmosphere is good. The haircut was a success!",
+    content: "I asked for a gradual fade, and they gradually faded my hair… Luckily, Non Stop Gym was just a stone's throw away, so I was able       to wipe away the last drops of water that hadn't fallen from my eyes. I can't wait to be judged by my in-laws tonight. I don't recommend it      unless you like wearing a 2mm layer that goes all the way up your head… The hairdresser is still friendly, though.",
     rating: 5,
-    image: ""
+    image: "https://lh3.googleusercontent.com/a-/ALV-UjWkPeNdzyCDW11O4Y5TrMb00SsA74te0dvIEOOAgf_qgxVgbfBi=w90-h90-p-rp-mo-ba3-br100"
   },
+  
   {
-    id: 3,
+    id: 2,
     name: "Christinat Axel",
     role: "Client régulier",
     content: "Fast pick-up without reservation, impeccable and tailor-made cut! The barber team is super cool. I recommend",
@@ -33,7 +26,7 @@ export const testimonials: Testimonial[] = [
     image: "https://lh3.googleusercontent.com/a-/ALV-UjV8zI4SO72a-cJlcasp0XTqL37d9-5o87ibFC3GMAAVRsA7XqE=w90-h90-p-rp-mo-br100"
   },
   {
-    id: 1,
+    id: 3,
     name: "Abdellatif ROUCHDI",
     role: "Client régulier",
     content: "Best barber in Lausanne. Quick and always with smile!",
@@ -41,7 +34,7 @@ export const testimonials: Testimonial[] = [
     image: "https://lh3.googleusercontent.com/a-/ALV-UjXx00GKpcwIhBQu-qZJxih_1zyEp9BynbXDTZyQrz14-XlllfWlZg=w90-h90-p-rp-mo-br100"
   },
   {
-    id: 1,
+    id: 4,
     name: "Trey Davis",
     role: "Client régulier",
     content: "The gentleman is very friendly and the atmosphere is good. The haircut was a success!",
@@ -49,7 +42,7 @@ export const testimonials: Testimonial[] = [
     image: ""
   },
   {
-    id: 1,
+    id: 5,
     name: "Daniel Cardoso",
     role: "Client régulier",
     content: "Top notch, friendly barbers. Top notch service.",
