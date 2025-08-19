fa5fb0ff140aca757019eff90bcc36495f3b7660
Ilir-Prog <iliri.isufi@gmail.com>
2025-08-20 00:54:15 +0200

    Updated testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -11,7 +11,7 @@ export const testimonials: Testimonial[] = [
   {
     id: 2,
     name: "Gauthier R",
-    role: "",
+    role: "Client régulier",
     content: "I asked for a gradual fade, and they gradually faded my hair… Luckily, Non Stop Gym was just a stone's throw away, so I was able       to wipe away the last drops of water that hadn't fallen from my eyes. I can't wait to be judged by my in-laws tonight. I don't recommend it      unless you like wearing a 2mm layer that goes all the way up your head… The hairdresser is still friendly, though.",
     rating: 5,
     image: "https://lh3.googleusercontent.com/a-/ALV-UjWkPeNdzyCDW11O4Y5TrMb00SsA74te0dvIEOOAgf_qgxVgbfBi=w90-h90-p-rp-mo-ba3-br100"
@@ -26,9 +26,9 @@ export const testimonials: Testimonial[] = [
   },
   {
     id: 3,
-    name: "Jean-Pierre Martin",
-    role: "Homme d'affaires",
-    content: "Barbier de confiance depuis 3 ans. Toujours satisfait du résultat. Service haut de gamme.",
+    name: "Christinat Axel",
+    role: "Client régulier",
+    content: "Fast pick-up without reservation, impeccable and tailor-made cut! The barber team is super cool. I recommend",
     rating: 5,
     image: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&fit=crop"
   }
