dce658b4a2f3effbcfc1cf363f8690a643a5953f
Ilir-Prog <iliri.isufi@gmail.com>
2025-08-20 00:51:43 +0200

    Updated testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -22,7 +22,7 @@ export const testimonials: Testimonial[] = [
     role: "",
     content: "I asked for a gradual fade, and they gradually faded my hair… Luckily, Non Stop Gym was just a stone's throw away, so I was able to wipe away the last drops of water that hadn't fallen from my eyes. I can't wait to be judged by my in-laws tonight. I don't recommend it unless you like wearing a 2mm layer that goes all the way up your head… The hairdresser is still friendly, though.",
     rating: 5,
-    image: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&fit=crop"
+    image: "https://lh3.googleusercontent.com/a-/ALV-UjWkPeNdzyCDW11O4Y5TrMb00SsA74te0dvIEOOAgf_qgxVgbfBi=w90-h90-p-rp-mo-ba3-br100"
   },
   {
     id: 3,
