831b1feb606e7a87a08bc1ad8f0d24511a107339
Ilir-Prog <iliri.isufi@gmail.com>
2025-08-20 00:59:08 +0200

    Updated testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -10,11 +10,11 @@ export interface Testimonial {
 export const testimonials: Testimonial[] = [
   {
     id: 2,
-    name: "Gauthier R",
+    name: "Walter Ferrari",
     role: "Client régulier",
-    content: "I asked for a gradual fade, and they gradually faded my hair… Luckily, Non Stop Gym was just a stone's throw away, so I was able       to wipe away the last drops of water that hadn't fallen from my eyes. I can't wait to be judged by my in-laws tonight. I don't recommend it      unless you like wearing a 2mm layer that goes all the way up your head… The hairdresser is still friendly, though.",
+    content: "Very efficient barber with a warm welcome, and a good price for a cut of CHF 25.- Ideal if you're looking for good value for money     in the center of Lausanne.",
     rating: 5,
-    image: "https://lh3.googleusercontent.com/a-/ALV-UjWkPeNdzyCDW11O4Y5TrMb00SsA74te0dvIEOOAgf_qgxVgbfBi=w90-h90-p-rp-mo-ba3-br100"
+    image: "https://lh3.googleusercontent.com/a-/ALV-UjUuwHljpOP57ZtJSpQlFyMdDBWC6ymoUgGLOKXVmrGuuIa1FzGA=w90-h90-p-rp-mo-ba4-br100"
   },
   {
     id: 1,
