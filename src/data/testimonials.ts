7e7ff03dde9d6ff8b415a4f984d12c65609ae763
Ilir-Prog <iliri.isufi@gmail.com>
2025-08-20 00:51:20 +0200

    Updated testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -18,9 +18,9 @@ export const testimonials: Testimonial[] = [
   },
   {
     id: 2,
-    name: "gAUTHIER r",
+    name: "Gauthier R",
     role: "",
-    content: "Excellent rapport qualité-prix pour les étudiants. Coupe moderne et service rapide. Parfait !",
+    content: "I asked for a gradual fade, and they gradually faded my hair… Luckily, Non Stop Gym was just a stone's throw away, so I was able to wipe away the last drops of water that hadn't fallen from my eyes. I can't wait to be judged by my in-laws tonight. I don't recommend it unless you like wearing a 2mm layer that goes all the way up your head… The hairdresser is still friendly, though.",
     rating: 5,
     image: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&fit=crop"
   },
